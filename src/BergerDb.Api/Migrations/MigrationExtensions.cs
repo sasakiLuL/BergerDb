0ad3ad3c1eeda1f21965b8b91a1903159c192a91
Sasha Kryvko <sashakrivko75@gmail.com>
2024-11-01 02:29:26 +0100

    Added basic api layer

---
+++ src/BergerDb.Api/Migrations/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BergerDb.Api.Migrations;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
+    {
+        using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+        using TContext context =
+            serviceScope.ServiceProvider.GetRequiredService<TContext>();
+
+        context.Database.Migrate();
+    }
+}
--- src/BergerDb.Api/Program.cs
+++ src/BergerDb.Api/Program.cs
@@ -1,16 +1,82 @@
+using BergerDb.Domain;
+using BergerDb.Application;
+using BergerDb.Persistance;
+using BergerDb.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Mapster;
+using BergerDb.Application.Users.Register;
+using BergerDb.Application.Users.Login;
+using BergerDb.Api.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDomain();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPersistance(builder.Configuration);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.ApplyMigrations<BergerDbContext>();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.UseHttpsRedirection();
 
+app.MapPost("api/register", async (
+    [FromBody] RegisterRequest request,
+    ISender sender,
+    CancellationToken cancellationToken = default) =>
+{
+    var command = request.Adapt<RegisterCommand>();
+
+    var result = await sender.Send(command, cancellationToken);
+
+    if (result.IsFailure)
+    {
+        return Results.BadRequest(result.Errors);
+    }
+
+    return Results.Ok(result.Value);
+});
+
+app.MapPost("api/login", async (
+    [FromBody] LoginRequest request,
+    ISender sender,
+    CancellationToken cancellationToken = default) =>
+{
+    var command = request.Adapt<LoginCommand>();
+
+    var result = await sender.Send(command, cancellationToken);
+
+    if (result.IsFailure)
+    {
+        return Results.BadRequest(result.Errors);
+    }
+
+    return Results.Ok(result.Value);
+});
+
+app.MapGet("api/test", () => 
+{
+    return Results.Ok("Hello");
+})
+.RequireAuthorization();
+
 app.Run();
+
+record RegisterRequest(string Email, string Password);
+
+record LoginRequest(string Email, string Password);
