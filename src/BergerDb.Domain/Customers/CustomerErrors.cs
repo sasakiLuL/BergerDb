d962433f56ad6d22af8b3c4351962e990ce3938c
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-16 15:34:23 +0200

    Added some customer errors

--- src/BergerDb.Domain/Customers/CustomerErrors.cs
+++ src/BergerDb.Domain/Customers/CustomerErrors.cs
@@ -1,6 +1,14 @@
-﻿namespace BergerDb.Domain.Customers;
+﻿using BergerDb.Shared.Errors;
+
+namespace BergerDb.Domain.Customers;
 
 public static class CustomerErrors
 {
+    public static Error NotFound => new(
+        "Customer.NotFound",
+        "The customer with specified id was not found.");
 
+    public static Error InvalidFieldName => new(
+        "Customer.InvalidFieldName",
+        "The field name is invalid.");
 }
