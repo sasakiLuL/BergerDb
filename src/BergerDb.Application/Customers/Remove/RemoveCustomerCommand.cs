3324a607098c98d63ba60bae3144903647f1b4cc
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-16 15:43:52 +0200

    Added remove customer command

---
+++ src/BergerDb.Application/Customers/Remove/RemoveCustomerCommand.cs
@@ -0,0 +1,5 @@
+﻿using BergerDb.Application.Abstractions.Messaging;
+
+namespace BergerDb.Application.Customers.Remove;
+
+public record RemoveCustomerCommand(Guid Id) : ICommand;
---
+++ src/BergerDb.Application/Customers/Remove/RemoveCustomerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using BergerDb.Application.Abstractions.Data;
+using BergerDb.Application.Abstractions.Messaging;
+using BergerDb.Domain.Customers;
+using BergerDb.Shared.Results;
+
+namespace BergerDb.Application.Customers.Remove;
+
+public class RemoveCustomerCommandHandler(
+    ICustomerRepository customerRepository,
+    IUnitOfWork unitOfWork) : ICommandHandler<RemoveCustomerCommand>
+{
+    public async Task<Result> Handle(RemoveCustomerCommand request, CancellationToken token)
+    {
+        var customer = await customerRepository.GetByIdAsync(request.Id, token);
+
+        if (customer is null)
+        {
+            return Result.Failure(CustomerErrors.NotFound);
+        }
+
+        customerRepository.Delete(customer);
+
+        await unitOfWork.CommitAsync(token);
+
+        return Result.Success();
+    }
+}
