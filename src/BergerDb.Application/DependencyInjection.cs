8e8ba3d88db6807585ed6b5b5c28a2ea75139bbc
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-16 15:52:08 +0200

    Added application dependency injection. Updated project dotnet version and added some references

---
+++ src/BergerDb.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BergerDb.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddMediatR((config) =>
+        {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
