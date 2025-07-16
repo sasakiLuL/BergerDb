b21538a1b0045fd741ee76a75d2a7520c30ad3c2
Sasha Kryvko <sashakrivko75@gmail.com>
2025-07-16 15:43:40 +0200

    Added get customer by id query

---
+++ src/BergerDb.Application/Customers/GetById/GetCustomerByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using BergerDb.Application.Abstractions.Messaging;
+
+namespace BergerDb.Application.Customers.GetById;
+
+public record GetCustomerByIdQuery(
+    Guid Id) : IQuery<CustomerResponse>;
---
+++ src/BergerDb.Application/Customers/GetById/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BergerDb.Application.Abstractions.Messaging;
+using BergerDb.Domain.Customers;
+using BergerDb.Shared.Results;
+
+namespace BergerDb.Application.Customers.GetById;
+
+public class GetCustomerByIdQueryHandler(
+    ICustomerRepository customerRepository,
+    IMapper mapper) : IQueryHandler<GetCustomerByIdQuery, CustomerResponse>
+{
+    public async Task<Result<CustomerResponse>> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+    {
+        var customer = await customerRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (customer is null)
+        {
+            return Result.Failure<CustomerResponse>(CustomerErrors.NotFound);
+        }
+
+        return Result.Success(mapper.Map<CustomerResponse>(customer));
+    }
+}
