2b8f0d487fb69b7c3b6b72ed27979851d25c4382
sasakiLuL <sashakrivko75@gmail.com>
2025-09-25 15:50:34 +0200

    Add main file

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "Views/MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Ui::Views::MainWindow w;
+    w.show();
+    return a.exec();
+}
