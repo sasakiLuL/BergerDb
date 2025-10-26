d41fe6f303ee721e1cbb0902df77d61716c5b3c4
sasakiLuL <sashakrivko75@gmail.com>
2025-10-26 13:07:15 +0100

    Update main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
-#include "Views/MainWindow.h"
 #include <QApplication>
+#include <QFont>
+
+#include <DbClient.hpp>
+#include <DbTablesService.hpp>
+#include <AppStateController.hpp>
+#include <AppWindow.hpp>
+#include <States/FatalErrorState.hpp>
+#include <States/InitializationState.hpp>
+#include <States/NormalState.hpp>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Ui::Views::MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("BergerDb");
+    QCoreApplication::setApplicationName("BergerDb");
+
+    QFont globalFont("Arial", 12);
+    globalFont.setBold(true);
+    app.setFont(globalFont);
+
+    auto dbClient = Infrastructure::DbClient{"berger.db", &app};
+    auto dbTablesService = Infrastructure::DbTablesService{&dbClient, ":/resources/sql/main_tables.sql", &app};
+
+    auto appStateController = Presentation::AppStateController{&app};
+
+    auto appWindow = Presentation::AppWindow{&appStateController};
+
+    auto fatalErrorState = Presentation::FatalErrorState{&appStateController, &appWindow};
+    auto initializingState = Presentation::InitializationState{&appStateController, &fatalErrorState, &dbTablesService, &appWindow};
+    auto normalState = Presentation::NormalState{&appStateController, &appWindow};
+
+    appStateController.registerState(Presentation::AppState::Initialization, &initializingState);
+    appStateController.registerState(Presentation::AppState::Normal, &normalState);
+    appStateController.registerState(Presentation::AppState::FatalError, &fatalErrorState);
+
+    appStateController.showState(Presentation::AppState::Initialization);
+
+    appWindow.show();
+    return app.exec();
 }
